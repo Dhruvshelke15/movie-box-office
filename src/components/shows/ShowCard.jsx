983e37610ad3b65b529be52998399543582303ee
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-07-20 15:48:52 +0530

    modified star function

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,4 +1,4 @@
-const ShowCard = ({ name, image, id, summary, onStarMeClicked }) => {
+const ShowCard = ({ name, image, id, summary, onStarMeClicked, isStarred }) => {
   const summaryCut = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
     : 'No description';
@@ -14,7 +14,7 @@ const ShowCard = ({ name, image, id, summary, onStarMeClicked }) => {
           Read More
         </a>
         <button type="button" onClick={() => onStarMeClicked(id)}>
-          Star me
+          {isStarred ? 'Unstar Me' : 'Star Me'}
         </button>
       </div>
     </div>
